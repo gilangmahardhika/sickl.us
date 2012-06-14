a151c51ed93ccd118421b2dd9fe63f8750810c1c
Gilang Putera Mahardhika <gilangmahardhika@gmail.com>
2012-06-15 00:07:50 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sickl::Application.config.secret_token = 'cbda4d87411d22dacd1802a3902555f95f1ef2d1a0606985c0b007d4a01ca23bba5bf56ea1b417a70c4956b422201cb9b1ef69b239bf4cdea218e017361e1f55'
